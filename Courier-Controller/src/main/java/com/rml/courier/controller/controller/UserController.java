811c5c701347d7512b816072228bceda8434171e
Tharuke <tharuke.lnbti@gmail.com>
2020-01-14 16:52:24 +0530

    user controller updated (partial update from hsenid)

--- Courier-Controller/src/main/java/com/rml/courier/controller/controller/UserController.java
+++ Courier-Controller/src/main/java/com/rml/courier/controller/controller/UserController.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Slf4j
 @RequestMapping("/api")
 @RestController
@@ -26,6 +29,16 @@ public class UserController {
         User savedUser = userService.saveRegistration(user);
         log.info("[User Controller] ==> Returning saved user with id [{}]", user.getId());
         return ResponseEntity.ok(savedUser);
+    }
+
+    @GetMapping("/user/getExistEmail/{email}")
+    public ResponseEntity<Map<String,Object>> findExistEmail(@PathVariable("email") String email) {
+        log.info("[User Controller] ==> Validating user email [{}]", email);
+        boolean status = userService.checkUserByEmail(email);
+
+        Map<String, Object> validatedResponse = new HashMap<>();
+        validatedResponse.put("response", status);
+
     }
 
 }
